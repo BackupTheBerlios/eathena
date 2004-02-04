089600927419e6a0de0e1bfcb4997d7cf8fd53b3
rovert <rovert>
2004-02-04 15:38:08 +0000

    772

--- athenaj/map/map.c
+++ athenaj/map/map.c
@@ -1,4 +1,4 @@
-// $Id: map.c,v 1.7 2004/02/01 22:31:43 rovert Exp $
+// $Id: map.c,v 1.8 2004/02/04 15:38:09 rovert Exp $
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -776,6 +776,7 @@ int map_quit(struct map_session_data *sd)
 	pc_stop_walking(sd,0);
 	pc_stopattack(sd);
 	pc_delghosttimer(sd);
+	pc_delgvg_ghosttimer(sd);
 	pc_delspiritball(sd,sd->spiritball,1);
 	skill_gangsterparadise(sd,0);
 
