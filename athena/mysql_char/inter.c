5b37f9815cf84ea34a6434b218343068076c167c
rovert <rovert>
2004-02-06 03:42:24 +0000

    777 Guild War Addition

--- athena/mysql_char/inter.c
+++ athena/mysql_char/inter.c
@@ -1,48 +1,37 @@
-//
-// original code from athena
-// SQL conversion by Jioh L. Jung
-//
-
+#include "mmo.h"
 #include "char.h"
+#include "socket.h"
+#include "timer.h"
 #include <string.h>
 #include <stdlib.h>
 
+#include "inter.h"
+#include "int_party.h"
+#include "int_guild.h"
+#include "int_storage.h"
+#include "int_pet.h"
 
-#define WISLIST_TTL	(30*1000)	// Wisper list TTL (30sec)
+#define WISLIST_TTL	(30*1000)	// Wisリストの生存時間(30秒)
 
-
-MYSQL mysql_handle;
-MYSQL_RES* 	sql_res ;
-MYSQL_ROW	sql_row ;
-int sql_fields, sql_cnt;
-char tmp_sql[65535];
-
-int db_server_port = 3306;
-char db_server_ip[16] = "127.0.0.1";
-char db_server_id[32] = "ragnarok";
-char db_server_pw[32] = "ragnarok";
-char db_server_logindb[32] = "ragnarok";
-
-
-// sending packet list
+// 送信パケット長リスト
 int inter_send_packet_length[]={
-	-1,-1, 27, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
+	-1,-1,27, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
 	-1, 7, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
-	35,-1, 11,15, 34,29, 7,-1,  0, 0, 0, 0,  0, 0,  0, 0,
-	10,-1, 15, 0, 79,19, 7,-1,  0,-1,-1,-1, 14,67,186,-1,
-	 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
+	35,-1,11,15, 34,29, 7,-1,  0, 0, 0, 0,  0, 0,  0, 0,
+	10,-1,15, 0, 79,19, 7,-1,  0,-1,-1,-1, 14,67,186,-1,
+	 8, 8, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
 	 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
 	 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
 	 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
 	11,-1, 7, 3,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
 };
-// recv. packet list
+// 受信パケット長リスト
 int inter_recv_packet_length[]={
 	-1,-1, 5, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
 	 6,-1, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
 	72, 6,52,14, 10,29, 6,-1, 34, 0, 0, 0,  0, 0,  0, 0,
 	-1, 6,-1, 0, 55,19, 6,-1, 14,-1,-1,-1, 14,19,186,-1,
-	 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
+	 4, 8, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
 	 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
 	 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
 	 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0,  0, 0,
@@ -51,7 +40,7 @@ int inter_recv_packet_length[]={
 
 struct WisList {
 	struct WisList* next;
-	int id, fd;
+	int id,fd;
 	int count;
 	unsigned long tick;
 	unsigned char src[24];
@@ -62,7 +51,7 @@ struct WisList {
 struct WisList *wis_list=NULL;
 short wis_id=0;
 
-// Wis list add
+// Wisリスト登録
 int add_wislist(struct WisList* list)
 {
 	
@@ -75,7 +64,7 @@ int add_wislist(struct WisList* list)
 		wis_id=0;
 	return list->id;
 }
-// Wis list search
+// Wisリスト検索
 struct WisList *search_wislist(int id)
 {
 	struct WisList* p;
@@ -85,12 +74,12 @@ struct WisList *search_wislist(int id)
 	}
 	return NULL;
 }
-// Wis list del.
+// Wisリスト削除
 int del_wislist(int id)
 {
 	struct WisList* p=wis_list, **prev=&wis_list;
 //	printf("del_wislist:start\n");
-	for( ; p; prev=&p->next, p=p->next ){
+	for( ; p; prev=&p->next,p=p->next ){
 		if( p->id == id ){
 			*prev=p->next;
 			free(p);
@@ -101,13 +90,13 @@ int del_wislist(int id)
 //	printf("del_wislist:not found\n");
 	return 0;
 }
-// Wis list alive check
+// Wisリストの生存チェック
 int check_ttl_wislist()
 {
 	unsigned long tick=gettick();
 	struct WisList* p=wis_list, **prev=&wis_list;
-	for( ; p; prev=&p->next, p=p->next ){
-		if( DIFF_TICK(tick, p->tick)>WISLIST_TTL ){
+	for( ; p; prev=&p->next,p=p->next ){
+		if( DIFF_TICK(tick,p->tick)>WISLIST_TTL ){
 			*prev=p->next;
 			free(p);
 			p=*prev;
@@ -119,176 +108,133 @@ int check_ttl_wislist()
 //--------------------------------------------------------
 
 /*==========================================
- * read config file
+ * 設定ファイルを読み込む
  *------------------------------------------
  */
-int inter_config_read(const char *cfgName) {
-	printf ("start reading interserver configuration: %s\n",cfgName);
+int inter_config_read(const char *cfgName)
+{
 	int i;
-	char line[1024], w1[1024], w2[1024];
+	char line[1024],w1[1024],w2[1024];
 	FILE *fp;
 
 	fp=fopen(cfgName,"r");
 	if(fp==NULL){
-		printf("file not found: %s\n", cfgName);
+		printf("file not found: %s\n",cfgName);
 		return 1;
 	}
-	while(fgets(line, 1020, fp)){
+	while(fgets(line,1020,fp)){
 		i=sscanf(line,"%[^:]: %[^\r\n]",w1,w2);
 		if(i!=2)
 			continue;
-
-		if(strcmpi(w1,"party_txt")==0){
-			printf ("set party_txt : %s\n",w2);
-			strncpy(party_txt, w2, sizeof(party_txt));
+		if(strcmpi(w1,"storage_txt")==0){
+			strncpy(storage_txt,w2,sizeof(storage_txt));
+		} else if(strcmpi(w1,"party_txt")==0){
+			strncpy(party_txt,w2,sizeof(party_txt));
 		} else if(strcmpi(w1,"guild_txt")==0){
-			printf ("set guild_txt : %s\n",w2);
-			strncpy(guild_txt, w2, sizeof(guild_txt));
-		} //add for DB connection
-		else if(strcmpi(w1,"db_server_ip")==0){
-			strcpy(db_server_ip, w2);
-			printf ("set db_server_ip : %s\n",w2);
-		}
-		else if(strcmpi(w1,"db_server_port")==0){
-			db_server_port=atoi(w2);
-			printf ("set db_server_port : %s\n",w2);
-		}
-		else if(strcmpi(w1,"db_server_id")==0){
-			strcpy(db_server_id, w2);
-			printf ("set db_server_id : %s\n",w2);
-		}
-		else if(strcmpi(w1,"db_server_pw")==0){
-			strcpy(db_server_pw, w2);
-			printf ("set db_server_pw : %s\n",w2);
-		}
-		else if(strcmpi(w1,"db_server_logindb")==0){
-			strcpy(db_server_logindb, w2);
-			printf ("set db_server_logindb : %s\n",w2);
+			strncpy(guild_txt,w2,sizeof(guild_txt));
+		} else if(strcmpi(w1,"pet_txt")==0){
+			strncpy(pet_txt,w2,sizeof(pet_txt));
+		} else if(strcmpi(w1,"castle_txt")==0){
+			strncpy(castle_txt,w2,sizeof(castle_txt));
 		}
 	}
 	fclose(fp);
-	
-	printf ("success reading interserver configuration\n");
 
 	return 0;
 }
 
-// save
-int inter_save() {
-	// party & guild is save to file
+// セーブ
+int inter_save()
+{
+	inter_storage_save();
 	inter_party_save();
 	inter_guild_save();
+	inter_pet_save();
 
 	return 0;
 }
 
-int inter_save_timer(int tid, unsigned int tick, int id, int data){
-	//printf ("interserver save count-tic...\n");
-	inter_save();
-	return 0;
-}
-
-// initialize
+// 初期化
 int inter_init(const char *file)
 {
-	int i;
-	
-	printf ("interserver initialize...\n");
 	inter_config_read(file);
-	
-	//DB connection initialized
-	mysql_init(&mysql_handle);
-	printf("Connect DB server.... (inter server)\n");
-	if(!mysql_real_connect(&mysql_handle, db_server_ip, db_server_id, db_server_pw,
-		db_server_logindb ,db_server_port, (char *)NULL, 0)) {
-			//pointer check
-			printf("%s\n",mysql_error(&mysql_handle));
-			exit(1);
-	}
-	else {
-		printf ("connect success! (inter server)\n");
-	}
-
 
 	inter_storage_init();
 	inter_party_init();
 	inter_guild_init();
-	inter_pet_sql_init();
-
-	printf ("interserver timer initializing : %d sec...\n",autosave_interval);
-	i=add_timer_interval(gettick()+autosave_interval,inter_save_timer,0,0,autosave_interval);
+	inter_pet_init();
 
 	return 0;
 }
 
 //--------------------------------------------------------
 
-// GM message sending
-int mapif_GMmessage(unsigned char *mes, int len)
+// GMメッセージ送信
+int mapif_GMmessage(unsigned char *mes,int len)
 {
 	unsigned char buf[len];
-	WBUFW(buf, 0) =0x3800;
-	WBUFW(buf, 2) =len;
-	memcpy(WBUFP(buf, 4), mes, len-4);
-	mapif_sendall(buf, len);
-	printf("inter server: GM[len:%d] - '%s'\n", len, mes);
+	WBUFW(buf,0)=0x3800;
+	WBUFW(buf,2)=len;
+	memcpy(WBUFP(buf,4),mes,len-4);
+	mapif_sendall(buf,len);
+//	printf("inter server: GM:%d %s\n",len,mes);
 	return 0;
 }
 
-// Wis sending
+// Wis送信
 int mapif_wis_message(struct WisList *wl)
 {
 	unsigned char buf[1024];
 	
-	WBUFW(buf, 0) =0x3801;
+	WBUFW(buf, 0)=0x3801;
 	WBUFW(buf, 2)=6 + 48 +wl->len;
-	WBUFW(buf, 4) =wl->id;
-	memcpy(WBUFP(buf, 6), wl->src, 24);
-	memcpy(WBUFP(buf, 30), wl->dst, 24);
-	memcpy(WBUFP(buf, 54), wl->msg, wl->len);
-	wl->count = mapif_sendall(buf, WBUFW(buf, 2));
+	WBUFW(buf, 4)=wl->id;
+	memcpy(WBUFP(buf, 6),wl->src,24);
+	memcpy(WBUFP(buf,30),wl->dst,24);
+	memcpy(WBUFP(buf,54),wl->msg,wl->len);
+	wl->count = mapif_sendall(buf,WBUFW(buf,2));
 //	printf("inter server wis: %d %d %ld\n", wl->id, wl->count, wl->tick);
 	return 0;
 }
-// Wis sending result
-int mapif_wis_end(struct WisList *wl, int flag)
+// Wis送信結果
+int mapif_wis_end(struct WisList *wl,int flag)
 {
 	unsigned char buf[32];
 	
-	WBUFW(buf, 0) =0x3802;
-	memcpy(WBUFP(buf, 2), wl->src, 24);
-	WBUFB(buf, 26) =flag;
-	mapif_send(wl->fd, buf, 27);
-//	printf("inter server wis_end %d\n", flag);
+	WBUFW(buf, 0)=0x3802;
+	memcpy(WBUFP(buf, 2),wl->src,24);
+	WBUFB(buf,26)=flag;
+	mapif_send(wl->fd,buf,27);
+//	printf("inter server wis_end %d\n",flag);
 	return 0;
 }
 
 //--------------------------------------------------------
 
-// GM message sending
+// GMメッセージ送信
 int mapif_parse_GMmessage(int fd)
 {
-	mapif_GMmessage(RFIFOP(fd, 4), RFIFOW(fd, 2));
+	mapif_GMmessage(RFIFOP(fd,4),RFIFOW(fd,2));
 	return 0;
 }
 
 
-// Wis sending request
+// Wis送信要求
 int mapif_parse_WisRequest(int fd)
 {
-	struct WisList* wl = (struct WisList *) malloc(sizeof(struct WisList));
+	struct WisList* wl = (struct WisList *)malloc(sizeof(struct WisList));
 	if(wl==NULL){
-		// Wis sending fail
-		RFIFOSKIP(fd, RFIFOW(fd, 2));
+		// Wis送信失敗（パケットを送る必要ありかも）
+		RFIFOSKIP(fd,RFIFOW(fd,2));
 		return 0;
 	}
 	check_ttl_wislist();
 	
-	wl->fd=fd;	// WisList set
-	memcpy(wl->src, RFIFOP(fd, 4), 24);
-	memcpy(wl->dst, RFIFOP(fd, 28), 24);
-	wl->len=RFIFOW(fd, 2)-52;
-	memcpy(wl->msg, RFIFOP(fd, 52), wl->len);
+	wl->fd=fd;	// WisListセット
+	memcpy(wl->src,RFIFOP(fd, 4),24);
+	memcpy(wl->dst,RFIFOP(fd,28),24);
+	wl->len=RFIFOW(fd,2)-52;
+	memcpy(wl->msg,RFIFOP(fd,52),wl->len);
 	
 	add_wislist(wl);
 	
@@ -296,20 +242,21 @@ int mapif_parse_WisRequest(int fd)
 	return 0;
 }
 
-// Wis sending result
+// Wis送信結果
 int mapif_parse_WisReply(int fd)
 {
-	int id=RFIFOW(fd, 2), flag=RFIFOB(fd, 4);
+	int id=RFIFOW(fd,2),flag=RFIFOB(fd,4);
 	
 	struct WisList* wl=search_wislist(id);
 	
 	if(wl==NULL){
-		RFIFOSKIP(fd, 5);
-		return 0;	// no such ID
+		RFIFOSKIP(fd,5);
+		return 0;	// タイムアウトしたかIDが存在しない
 	}
 	
 	if((--wl->count)==0 || flag!=1){
-		mapif_wis_end(wl, flag);
+		// 全鯖終わったか、ある鯖から終了する結果が返った
+		mapif_wis_end(wl,flag);
 		del_wislist(wl->id);
 	}
 	
@@ -317,18 +264,22 @@ int mapif_parse_WisReply(int fd)
 }
 
 //--------------------------------------------------------
+
+// map server からの通信（１パケットのみ解析すること）
+// エラーなら0(false)、処理できたなら1、
+// パケット長が足りなければ2をかえさなければならない
 int inter_parse_frommap(int fd)
 {
-	int cmd=RFIFOW(fd, 0);
+	int cmd=RFIFOW(fd,0);
 	int len=0;
 
-	// inter packet?
+	// inter鯖管轄かを調べる
 	if(cmd<0x3000 || cmd>=0x3000+( sizeof(inter_recv_packet_length)/
 		sizeof(inter_recv_packet_length[0]) ) )
 		return 0;
 
-	// packet check
-	if(	(len=inter_check_length(fd, inter_recv_packet_length[cmd-0x3000]))==0 )
+	// パケット長を調べる
+	if(	(len=inter_check_length(fd,inter_recv_packet_length[cmd-0x3000]))==0 )
 		return 2;
 	
 	switch(cmd){
@@ -350,16 +301,17 @@ int inter_parse_frommap(int fd)
 	return 1;
 }
 
-// RFIFO check
-int inter_check_length(int fd, int length)
+// RFIFOのパケット長確認
+// 必要パケット長があればパケット長、まだ足りなければ0
+int inter_check_length(int fd,int length)
 {
-	if(length==-1){	// v-len packet
-		if(RFIFOREST(fd)<4)	// packet not yet
+	if(length==-1){	// 可変パケット長
+		if(RFIFOREST(fd)<4)	// パケット長が未着
 			return 0;
-		length = RFIFOW(fd, 2);
+		length = RFIFOW(fd,2);
 	}
 	
-	if(RFIFOREST(fd)<length)	// packet not yet
+	if(RFIFOREST(fd)<length)	// パケットが未着
 		return 0;
 	
 	return length;
