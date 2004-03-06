e16ca78654e131825d38ce54f344afab0f25f1d3
moonsoul <moonsoul>
2004-03-06 18:05:39 +0000

    change to pc_nexttojobexp to allow super novice and advanced 2nd classes to have up to job level 70

--- athena/map/pc.c
+++ athena/map/pc.c
@@ -3522,9 +3522,15 @@ int pc_nextjobexp(struct map_session_data *sd)
 	if(sd->status.job_level>=MAX_LEVEL || sd->status.job_level<=0)
 		return 0;
 
-	if(sd->status.class==0) i=4;
-	else if(sd->status.class<=6) i=5;
+//	if(sd->status.class==0) i=4;
+//	else if(sd->status.class<=6) i=5;
+//	else if(sd->status.class<23) i=6;
+//	else i=7;
+//
+	if(sd->status.class==0 || sd->status.class==4001) i=4;
+	else if(sd->status.class<=6 || (sd->status.class > 4001 && sd->status.class <=4007) i=5;
 	else if(sd->status.class<23) i=6;
+	else if(sd->status.class == 23 || sd->status.class > 4007) i=8;
 	else i=7;
 
 	return exp_table[i][sd->status.job_level-1];
