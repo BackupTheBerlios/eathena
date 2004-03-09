e869880baa11b175c6dcb72c8694b457ccbf46d4
mjflick <mjflick>
2004-03-09 17:41:00 +0000

    addition of not here

--- athena/webserver/generate.c
+++ athena/webserver/generate.c
@@ -7,6 +7,12 @@ void generate_page(int sock_in, char *query, char *ip)
 	//To make this simple, we will have a bunch of if statements
 	//that then shoot out data off into functions.
 
+	
+	//The 'index'
+	if ( strcmp(page, "/") == 0 )
+		generate_notdone(sock_in, query, ip);
+
+
 	//About page:
 	if ( strcmp(page, "/about.html") == 0 )
 		generate_about(sock_in, query, ip);
